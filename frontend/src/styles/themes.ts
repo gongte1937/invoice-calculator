c6d74e673b610c0060a4b7df5fa09c10bb5baa83
Terry <gongte1937@gmail.com>
2025-09-27 15:14:43 +0930

    bootstrap quarkus project

--- frontend/src/styles/themes.ts
+++ frontend/src/styles/themes.ts
@@ -3,8 +3,8 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: { main: "#1976d2" }, // 蓝色
-    secondary: { main: "#9c27b0" }, // 紫色
+    primary: { main: "#1976d2" },
+    secondary: { main: "#9c27b0" },
   },
 });
 
